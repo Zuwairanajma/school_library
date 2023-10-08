7182495cc968f8d26e5451bb863d0dbdb922a363
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-08 15:45:08 +0100

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+class Teacher < Person
+    attr_accessor :specialization
+
+    def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+        super(age, name, parent_permission)
+        @specialization = specialization
+    end
+
+    def can_use_services?
+        return true
+    end
+end
