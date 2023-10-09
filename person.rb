f3c1a4e16b5b1ed086fc0821955a82f6c831fce3
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-09 16:55:05 +0100

    Add the association requirements

--- person.rb
+++ person.rb
@@ -2,6 +2,8 @@ require_relative 'nameable'
 require_relative 'decorator'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+# require_relative 'classroom'
+# require_relative 'student'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -29,6 +31,10 @@ class Person < Nameable
   def of_age?
     age >= 18
   end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
 end
 person = Person.new(22, 'maximilianus')
 person.correct_name
@@ -36,3 +42,20 @@ capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
+
+# Example Usage:
+
+# classroom_a = Classroom.new("Class A")
+# classroom_b = Classroom.new("Class B")
+
+# student_1 = Student.new(18, "John")
+# student_2 = Student.new("Alice")
+
+# classroom_a.add_student(student_1)
+# classroom_b.add_student(student_2)
+
+# puts "Classroom A Students: #{classroom_a.students.map(&:name).join(', ')}"
+# puts "Classroom B Students: #{classroom_b.students.map(&:name).join(', ')}"
+
+# puts "#{student_1.name} is in #{student_1.classroom&.label}"
+# puts "#{student_2.name} is in #{student_2.classroom&.label}"
--- student.rb
+++ student.rb
@@ -2,12 +2,19 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self)
+    unless classroom.students.include?
+      (self)
+    end
 
   def play_hooky
     '¯\\(ツ)/¯'
