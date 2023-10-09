9416c9139f0978aa162d8e46e3ca96515d42bea0
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-09 07:02:34 +0100

    Change @id value for person class

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = SecureRandom.hex(4)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
