ea35139ef1c30a3e5a775368f9d42cf12805da5a
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-09 07:53:51 +0100

    Update person class to include decorate classes

--- person.rb
+++ person.rb
@@ -1,12 +1,25 @@
-class Person
+require_relative 'nameable'
+require_relative 'decorator'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = SecureRandom.hex(4)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    name
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
@@ -14,10 +27,10 @@ class Person
   def of_age?
     age >= 18
   end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
 end
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
