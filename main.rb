ffc941173d0e77d9736eaa23664bc82cc8475f84
firstpj <114886262+firstpj@users.noreply.github.com>
2023-10-16 13:10:06 +0100

    add update main.rb file

--- main.rb
+++ main.rb
@@ -7,23 +7,23 @@ class Main
   include MenuMethods
   include Options
   include Introductions
-  
+
   def initialize
-  @app = App.new
+    @app = App.new
   end
-  
+
   def run
     welcome_message
     loop do
-    show_menu
-    selected = getuser_input
-    break if selected == 7
+      show_menu
+      selected = getuser_input
+      break if selected == 7
 
-    select_option(selected, @app)
+      select_option(selected, @app)
+    end
+    exit_message
   end
- exit_message
-end
 end
 
 main = Main.new
-main.run 
+main.run
